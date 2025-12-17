2835c695ea4a63e85a663271f2d1db816cd9e0d5
Proslawa <127397919+proslawa@users.noreply.github.com>
2025-12-17 00:19:55 +0000

    Simplify database connection setup
    Replaced database connection configuration with a direct URL.

--- Traitement_Streaming/dashboard/dashboard_vf.py
+++ Traitement_Streaming/dashboard/dashboard_vf.py
@@ -10,14 +10,8 @@ import numpy as np
 from PIL import Image
 
 # ================= CONFIG =================
-DB_USER = st.secrets["DB_USER"]
-DB_PASSWORD = st.secrets["DB_PASSWORD"]
-DB_HOST = st.secrets["DB_HOST"]
-DB_PORT = st.secrets["DB_PORT"]
-DB_NAME = st.secrets["DB_NAME"]
-
-DB_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-engine = create_engine(DB_URL)
+DB_URL = "postgresql+psycopg2://sqlpad:sqlpad@localhost:5432/sqlpad"
+engine = create_engine(DB_URL, pool_pre_ping=True)
 
 st.set_page_config(
     page_title="📊 Crypto Dashboard",
